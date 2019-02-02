7ca5577a01ae62fe7f00284fe250c36f90ab770b
DarkEyeDragon <joshua.tack@student.vives.be>
2019-02-02 16:15:52 +0100

    Started implementation of Excel integration

---
+++ TimerSystem/Excel/Excel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace TimerSystem.Excel
+{
+    class Excel
+    {
+        public string Path { get; set; }
+        private readonly _Application _excel = new Application();
+        private Workbook workBook;
+        private readonly Worksheet _workSheet;
+
+        public Excel(string path, int sheet)
+        {
+            Path = path;
+            workBook = _excel.Workbooks.Open(path);
+            _workSheet = workBook.Worksheets[sheet];
+        }
+
+
+        public void WriteCell(int row, int column, string value)
+        {
+            _workSheet.Cells[row, column] = value;
+            
+        }
+
+        public void Save()
+        {
+            workBook.Save();
+        }
+    }
+}
---
+++ TimerSystem/Util/Regex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerSystem.Util
+{
+    class Regex
+    {
+        public static string RemoveIntegers(string input)
+        {
+            return System.Text.RegularExpressions.Regex.Replace(input, @"[\d]", string.Empty);
+        }
+    }
+}
